0ed48564f9167652039be1309c5c25c4aa055e39
kpblachut <kpblachut@gmail.com>
2025-01-18 17:01:01 +0100

    Implementacja GUI

--- src/main/java/org/AAKB/client/board/Field.java
+++ src/main/java/org/AAKB/client/board/Field.java
@@ -1,124 +1,124 @@
-//package org.AAKB.client.board;
-//
-//import org.AAKB.constants.PlayerColor;
-//
-//import javafx.scene.paint.*;
-//import javafx.scene.shape.Circle;
-//import javafx.scene.shape.StrokeType;
-//
-//public class Field {
-//    private int x;                                  // Współrzędna X pola (kolumna)
-//    private int y;                                  // Współrzędna Y pola (wiersz)
-//    private Circle circle;                          // Referencja do odpowiadającego Circle w GUI
-//    private PlayerColor color = PlayerColor.NONE;   // Kolor pionka na danym polu
-//
-//    public Field( int x, int y, Circle circle )
-//    {
-//        this.x = x;
-//        this.y = y;
-//        this.circle = circle;
-//    }
-//
-//    public int getX()
-//    {
-//        return this.x;
-//    }
-//
-//    public int getY()
-//    {
-//        return this.y;
-//    }
-//
-//    void setColor( PlayerColor color )
-//    {
-//        this.color = color;
-//        Stop[] gradientPhases = getProperGradient( color );
-//        setGradient( gradientPhases );
-//    }
-//
-//    private Stop[] getProperGradient( PlayerColor color )
-//    {
-//        Stop[] stops = { new Stop(0, Color.WHITE), null };
-//
-//        switch( color )
-//        {
-//            case NONE:
-//                this.color = PlayerColor.NONE;
-//                stops[ 1 ] = new Stop( 1, Color.WHITE );
-//                break;
-//            case RED:
-//                stops[ 1 ] = new Stop( 1, Color.RED );
-//                break;
-//            case GREEN:
-//                stops[ 1 ] = new Stop( 1, Color.GREEN );
-//                break;
-//            case YELLOW:
-//                stops[ 1 ] = new Stop( 1, Color.YELLOW );
-//                break;
-//            case BLUE:
-//                stops[ 1 ] = new Stop( 1, Color.BLUE );
-//                break;
-//            case ORANGE:
-//                stops[ 1 ] = new Stop( 1, Color.ORANGE );
-//                break;
-//            case VIOLET:
-//                stops[ 1 ] = new Stop( 1, Color.VIOLET );
-//                break;
-//            default:
-//                throw new RuntimeException( "Podany kolor nie istnieje: '" + color + "'" );
-//        }
-//        return stops;
-//    }
-//
-//    private void setGradient( Stop[] gradientStops )
-//    {
-//        RadialGradient gradient = new RadialGradient( 0, 0,
-//                0.5, 0.5, 0.5, true, CycleMethod.NO_CYCLE, gradientStops );
-//
-//        circle.setFill( gradient );
-//    }
-//
-//    PlayerColor getColor()
-//    {
-//        return color;
-//    }
-//
-//    void setSelected( boolean state )
-//    {
-//        StrokeType strokeType;
-//        if( !circle.isDisabled() )
-//        {
-//            if( state )
-//                strokeType = StrokeType.OUTSIDE; // lekko powiększa kółko
-//            else
-//                strokeType = StrokeType.INSIDE;
-//
-//            circle.setStrokeType( strokeType );
-//        }
-//
-//    }
-//
-//    public boolean circleEquals( Circle circle )
-//    {
-//        return this.circle == circle;
-//    }
-//
-//    void markAsPossibleJumpTarget( boolean state )
-//    {
-//        if( !circle.isDisabled() && color == PlayerColor.NONE )
-//        {
-//            Paint fillColor;
-//            if( state )
-//                fillColor = Paint.valueOf( "#47F2FF" );
-//            else
-//                fillColor = Color.WHITE;
-//
-//            circle.setFill( fillColor );
-//        }
-//    }
-//
-//    boolean isDisabled()
-//    {
-//        return circle.isDisabled();
-//    }
-//}
+package org.AAKB.client.board;
+
+import org.AAKB.constants.PlayerColor;
+
+import javafx.scene.paint.*;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
+
+public class Field {
+    private int x;                                  // Współrzędna X pola (kolumna)
+    private int y;                                  // Współrzędna Y pola (wiersz)
+    private Circle circle;                          // Referencja do odpowiadającego Circle w GUI
+    private PlayerColor color = PlayerColor.NONE;   // Kolor pionka na danym polu
+
+    public Field( int x, int y, Circle circle )
+    {
+        this.x = x;
+        this.y = y;
+        this.circle = circle;
+    }
+
+    public int getX()
+    {
+        return this.x;
+    }
+
+    public int getY()
+    {
+        return this.y;
+    }
+
+    void setColor( PlayerColor color )
+    {
+        this.color = color;
+        Stop[] gradientPhases = getProperGradient( color );
+        setGradient( gradientPhases );
+    }
+
+    private Stop[] getProperGradient( PlayerColor color )
+    {
+        Stop[] stops = { new Stop(0, Color.WHITE), null };
+
+        switch( color )
+        {
+            case NONE:
+                this.color = PlayerColor.NONE;
+                stops[ 1 ] = new Stop( 1, Color.WHITE );
+                break;
+            case RED:
+                stops[ 1 ] = new Stop( 1, Color.RED );
+                break;
+            case GREEN:
+                stops[ 1 ] = new Stop( 1, Color.GREEN );
+                break;
+            case YELLOW:
+                stops[ 1 ] = new Stop( 1, Color.YELLOW );
+                break;
+            case BLUE:
+                stops[ 1 ] = new Stop( 1, Color.BLUE );
+                break;
+            case ORANGE:
+                stops[ 1 ] = new Stop( 1, Color.ORANGE );
+                break;
+            case VIOLET:
+                stops[ 1 ] = new Stop( 1, Color.VIOLET );
+                break;
+            default:
+                throw new RuntimeException( "Podany kolor nie istnieje: '" + color + "'" );
+        }
+        return stops;
+    }
+
+    private void setGradient( Stop[] gradientStops )
+    {
+        RadialGradient gradient = new RadialGradient( 0, 0,
+                0.5, 0.5, 0.5, true, CycleMethod.NO_CYCLE, gradientStops );
+
+        circle.setFill( gradient );
+    }
+
+    public PlayerColor getColor()
+    {
+        return color;
+    }
+
+    void setSelected( boolean state )
+    {
+        StrokeType strokeType;
+        if( !circle.isDisabled() )
+        {
+            if( state )
+                strokeType = StrokeType.OUTSIDE; // lekko powiększa kółko
+            else
+                strokeType = StrokeType.INSIDE;
+
+            circle.setStrokeType( strokeType );
+        }
+
+    }
+
+    public boolean circleEquals( Circle circle )
+    {
+        return this.circle == circle;
+    }
+
+    void markAsPossibleJumpTarget( boolean state )
+    {
+        if( !circle.isDisabled() && color == PlayerColor.NONE )
+        {
+            Paint fillColor;
+            if( state )
+                fillColor = Paint.valueOf( "#47F2FF" );
+            else
+                fillColor = Color.WHITE;
+
+            circle.setFill( fillColor );
+        }
+    }
+
+    boolean isDisabled()
+    {
+        return circle.isDisabled();
+    }
+}
---
+++ src/main/java/org/AAKB/constants/Response.java
@@ -0,0 +1,34 @@
+package org.AAKB.constants;
+
+/**
+ * Klasa reprezentuje komunikaty pomiędzy serwerem a klientem
+ */
+public class Response
+{
+    private String code;
+    private int[] numbers;
+    private String[] words;
+
+    Response(String code, int[] numbers, String[] words)
+    {
+        this.code = code;
+        this.numbers = numbers;
+        this.words = words;
+    }
+
+    public String getCode()
+    {
+        return code;
+    }
+
+    public int[] getNumbers()
+    {
+        return numbers;
+    }
+
+    public String[] getWords()
+    {
+        return words;
+    }
+
+}
